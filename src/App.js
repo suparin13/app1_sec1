f9082124da6ee8cb4c4a6dae077151369b30478b
suparin13 <152370501+suparin13@users.noreply.github.com>
2025-02-20 12:03:14 +0700

    create form-select-file.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import FormText from "./form-text";
+import FormSelectFile from "./form-select-file";
 
 export default function App(){
-  return <FormText/>
+  return <FormSelectFile/>
 }
